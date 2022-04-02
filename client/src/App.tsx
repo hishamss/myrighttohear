7fc60ac6bb097becbcc0d4070678b412c9003ff1
hisham saymeh <hishamss90@gmail.com>
2022-04-02 11:47:35 -0700

    add footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header'
 import Body from './components/Body'
+import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Body/>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
