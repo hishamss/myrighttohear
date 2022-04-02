a1aaf507088a455bcaa1b4f2afeb15081feb917e
hisham saymeh <hishamss90@gmail.com>
2022-04-02 10:45:09 -0700

    add main container

---
+++ client/src/components/Body/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap'
+import './index.css'
+function Body() {
+    return (
+<Container fluid className="main-container">
+    Body
+</Container>
+    )
+}
+
+export default Body
