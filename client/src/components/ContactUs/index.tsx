b52efb20f584e149653202036b1f346466fa12d6
hisham saymeh <hishamss90@gmail.com>
2022-06-24 12:01:34 -0700

    add email functionality

--- client/src/components/ContactUs/index.tsx
+++ client/src/components/ContactUs/index.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { Container, Form, Button } from "react-bootstrap"
+import { send } from "@emailjs/browser";
 import './index.css';
 
 function ContactUs() {
+  const serviceID = process.env.REACT_APP_SERVICE_ID || "";
+  const templateID = process.env.REACT_APP_TEMPLATE_ID || "";
+  const publicKey = process.env.REACT_APP_PUBLIC_KEY || "";
   const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [affiliation, setAffiliation] = useState("");
+  const [name, setName] = useState("Not Provided");
+  const [affiliation, setAffiliation] = useState("Not Provided");
   const [help, setHelp] = useState("");
   const [helpMessage, setHelpMessage] = useState(false);
   const [emailMessage, setEmailMessage] = useState(false);
@@ -16,7 +20,26 @@ function ContactUs() {
     setHelpMessage(false);
     if (!email) setEmailMessage(true);
     if (!help) setHelpMessage(true);
-    console.log(email, name, affiliation, help)
+    let templateParams = {
+      from_name: name,
+      from_email: email,
+      from_affiliation: affiliation,
+      help_needed: help
+    }
+    if (email && help) {
+      send(serviceID, templateID, templateParams, publicKey)
+        .then(response => {
+          console.log("success", response.status, response.text);
+          setEmail("");
+          setHelp("");
+        }, (err) => {
+          console.log("failed", err);
+        });
+    }
+
+
+
+
 
   }
   return (
@@ -31,7 +54,7 @@ function ContactUs() {
         </p>
         <Form id="contactus-form" onSubmit={e => handleContactUsSubmit(e)}>
           <Form.Group className="contactus-from-group"><Form.Label>Email <span style={{ color: "red" }}>*</span></Form.Label>
-            <Form.Control name="email" type="email" onChange={(e) => {
+            <Form.Control name="email" type="email"  value={email} onChange={(e) => {
               setEmailMessage(false);
               setEmail(e.target.value);
 
@@ -46,7 +69,7 @@ function ContactUs() {
           <Form.Group className="contactus-from-group"><Form.Label>Affiliation</Form.Label>
             <Form.Control name="affiliation" type="text" onChange={(e) => setAffiliation(e.target.value)} /></Form.Group>
           <Form.Group className="contactus-from-group"><Form.Label>How Can We Help? <span style={{ color: "red" }}>*</span></Form.Label>
-            <Form.Control name="help" type="text" onChange={(e) => {
+            <Form.Control name="help" type="text" value={help} onChange={(e) => {
               setHelpMessage(false);
               setHelp(e.target.value);
 
